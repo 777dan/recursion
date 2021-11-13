7270a77aa7db3a895d570ac1eec826feaa22c0d4
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-11-13 10:31:26 +0200

    Add files via upload

--- recursion.js
+++ recursion.js
@@ -9,14 +9,18 @@ let m = prompt("Введите число для факториала");
 // document.write("<br>");
 
 function factorial(m) {
-    if (m != 1) return m * factorial(m - 1);
-    else {
-        return 1;
-    }
+    // if (m != 1) return m * factorial(m - 1);
+    // else {
+    //     return 1;
+    // }
+    return m != 1 ? m * factorial(m - 1) : 1;
 }
 
 function fib(n) {
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    // return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+    if (n <= 1) return n;
+    else {
+        return fib(n - 1) + fib(n - 2)
+    }
 }
-// let k = factorial(n);
 document.write("Факториал: " + factorial(m) + "<br>" + "Фибоначчи: " + fib(n));
