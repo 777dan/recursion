d60c63d830f8b1a040bf9cbce3210dd3647bd2de
Danoslo 255 <68803448+777dan@users.noreply.github.com>
2021-11-13 10:26:18 +0200

    Add files via upload

--- recursion.js
+++ recursion.js
@@ -1,17 +1,22 @@
-let n = prompt("Введите n");
-sum = 0;
-for (let i = 1; i <= n; i++) {
-    document.write(i * i + " ");
-    sum += i;
-}
-document.write(sum);
-document.write("<br>");
+let n = prompt("Введите число для фибоначчи");
+let m = prompt("Введите число для факториала");
+// sum = 0;
+// for (let i = 1; i <= n; i++) {
+//     // document.write(i * i + " ");
+//     // sum += i;
+// }
+// document.write(sum);
+// document.write("<br>");
 
-function sumX2(n) {
-    if (n != 1) return n * sumX2(n - 1);
+function factorial(m) {
+    if (m != 1) return m * factorial(m - 1);
     else {
         return 1;
     }
 }
-let k = sumX2(n);
-document.write(k);
+
+function fib(n) {
+    return n <= 1 ? n : fib(n - 1) + fib(n - 2);
+}
+// let k = factorial(n);
+document.write("Факториал: " + factorial(m) + "<br>" + "Фибоначчи: " + fib(n));
